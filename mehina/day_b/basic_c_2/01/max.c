4c911c52e974e89e10553eef9ea209c58d3fd59e
Kiran Kabaso <kirankabaso@gmail.com>
2025-11-17 12:32:07 +0200

    Q1 implementation and exe

--- mehina/day_b/basic_c_2/01/max.c
+++ mehina/day_b/basic_c_2/01/max.c
@@ -1,1 +1,27 @@
-//
+/*------------------------------------------------------
+* Filename: [max.c]
+* Description: [print the max of two numbers]
+* Author: [Kiran Kabaso]
+-------------------------------------------------------*/
+
+#include <stdio.h>
+
+/*------------------------------------------------------
+* Function Name - [main]
+*
+* Function Purpose - [print the max of two numbers]
+*
+* Parameters – NONE
+*
+* Return Values - [0]
+*
+* Author - [Kiran Kabaso]
+-------------------------------------------------------*/
+int main() {
+    int x = 5;
+    int y = 7;
+    int z = x > y ? x:y;
+    printf("z = %d", z);
+
+    return 0;
+}
