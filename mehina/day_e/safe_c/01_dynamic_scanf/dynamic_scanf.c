339ee0b1bdee790011605b54e81792ed7e89956f
Kiran Kabaso <kirankabaso@gmail.com>
2025-11-20 10:51:05 +0200

    day e safe_c 01_dynamic_scanf - fixed memory leak, added some gaurds against unsigned int to int conversion and changed input_char to be the proper type int

--- mehina/day_e/safe_c/01_dynamic_scanf/dynamic_scanf.c
+++ mehina/day_e/safe_c/01_dynamic_scanf/dynamic_scanf.c
@@ -12,7 +12,7 @@
 
 
 char* dynamic_scanf(){
-  char input_char = 0;
+  int input_char = 0;
 
   char* failed_return_value = NULL; //return this on failure
 
@@ -21,16 +21,21 @@ char* dynamic_scanf(){
     return failed_return_value;
   }
 
-  int current_array_size = 1;
-  int count_array_members = 0;
+  unsigned int current_array_size = 1;
+  unsigned int count_array_members = 0;
 
   do {
     input_char = getchar();
     if (input_char != NEW_LINE && input_char != EOF){
       if (count_array_members == current_array_size){//increase array size if needed
         //allocate a new array with more memory.
-        char* current_array_ptr_tmp = (char*)malloc(sizeof(char)*current_array_size*2);
+        if ((int)current_array_size < 0){ //check current_array_size will survive int conversion
+          free(current_array_ptr);
+          return failed_return_value;
+        }
+        char* current_array_ptr_tmp = (char*)malloc(sizeof(char)*((int)current_array_size)*2);
         if (current_array_ptr_tmp == NULL){
+          free(current_array_ptr);
           return failed_return_value;
         }
         //copy the values to the new array
@@ -39,10 +44,14 @@ char* dynamic_scanf(){
         }
         free(current_array_ptr);
         current_array_ptr = current_array_ptr_tmp;
-        current_array_size *= 2;
+        current_array_size *= 2U;
       }
 
-      *(current_array_ptr + count_array_members) = input_char;
+      if ((int)count_array_members < 0){ //check count_array_members will survive int conversion
+          free(current_array_ptr);
+          return failed_return_value;
+      }
+      *(current_array_ptr + (int)count_array_members) = input_char;
       ++count_array_members;
     }
 
@@ -51,8 +60,14 @@ char* dynamic_scanf(){
 
   //return the array without the extra spaces for more members.
   //construct a new array without the extra spaces. add 1 space for '\0'.
-  char *new_array_ptr = (char*)malloc(sizeof(char)*count_array_members + 1); //add 1 for '\0'
+
+  if ((int)count_array_members < 0){ //check count_array_members will survive int conversion
+    free(current_array_ptr);
+    return failed_return_value;
+  }
+  char *new_array_ptr = (char*)malloc(sizeof(char)*((int)count_array_members) + 1); //add 1 for '\0'
   if(new_array_ptr == NULL){
+    free(current_array_ptr);
     return failed_return_value;
   }
   //copy the values to the new array
